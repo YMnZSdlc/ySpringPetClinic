c2ba99645ae21840716ab4961089ce176004abb7
DESKTOP-95T3DT5\Michał Mielczarek <michal.mielczarek@gmail.com>
2018-09-18 21:52:22 +0200

    Initial Project Add

---
+++ src/main/java/pl/ymz/spring/yspringpetclinic/YSpringPetClinicApplication.java
@@ -0,0 +1,12 @@
+package pl.ymz.spring.yspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YSpringPetClinicApplication.class, args);
+    }
+}
