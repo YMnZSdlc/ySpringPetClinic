bde2fb141c018919bf9d36fb642554df3df3139b
YMnZSdlc <michal.mielczarek@gmail.com>
2020-01-26 15:27:55 +0100

    Implements Interfaces for services. Closes #5

---
+++ petclinic-data/src/main/java/y/spring/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package y.spring.petclinic.services;
+
+import y.spring.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
