5098ba3bb07b78beb4586cc89ea68589eb8e6e36
YMnZSdlc <michal.mielczarek@gmail.com>
2020-02-09 14:06:39 +0100

    List all Owners on Owners Index Page closes #16

--- petclinic-web/src/main/java/y/spring/petclinic/controllers/OwnerController.java
+++ petclinic-web/src/main/java/y/spring/petclinic/controllers/OwnerController.java
@@ -1,14 +1,30 @@
 package y.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import y.spring.petclinic.model.Owner;
+import y.spring.petclinic.services.OwnerService;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Owner::getId))
+                .collect(Collectors.toList()));
         return "owners/index";
     }
 }
