f89dd454abb96726821f567df0e08dfa9778296b
YMnZSdlc <michal.mielczarek@gmail.com>
2020-02-09 15:40:29 +0100

    List of Vets on Vet index page closes #17

--- petclinic-web/src/main/java/y/spring/petclinic/controllers/VetController.java
+++ petclinic-web/src/main/java/y/spring/petclinic/controllers/VetController.java
@@ -1,13 +1,29 @@
 package y.spring.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import y.spring.petclinic.model.Vet;
+import y.spring.petclinic.services.VetService;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+        model.addAttribute("vets", vetService.findAll()
+                .stream()
+                .sorted(Comparator.comparing(Vet::getId))
+                .collect(Collectors.toList()));
         return "vets/index";
     }
 }
