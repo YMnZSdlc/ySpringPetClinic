e1e3949e116b3b541b056d0e024033e031008e91
YMnZSdlc <michal.mielczarek@gmail.com>
2020-02-09 18:44:17 +0100

    Update Map implementation to manage setting of ID Property closes #25

--- petclinic-web/src/main/java/y/spring/petclinic/bootstrap/DataLoader.java
+++ petclinic-web/src/main/java/y/spring/petclinic/bootstrap/DataLoader.java
@@ -24,19 +24,16 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Andrzej");
         owner1.setLastName("Andrzejewski");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Bartek");
         owner2.setLastName("Bartkowski");
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(3L);
         owner3.setFirstName("Celina");
         owner3.setLastName("Celińska");
         ownerService.save(owner3);
@@ -44,25 +41,20 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Daniel");
         vet1.setLastName("Danielski");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Edward");
         vet2.setLastName("Edwardzki");
         vetService.save(vet2);
 
         Vet vet3 = new Vet();
-        vet3.setId(3L);
         vet3.setFirstName("Filip");
         vet3.setLastName("Filipowski");
         vetService.save(vet3);
 
         System.out.println("Loaded Vets...");
-
-
     }
 }
